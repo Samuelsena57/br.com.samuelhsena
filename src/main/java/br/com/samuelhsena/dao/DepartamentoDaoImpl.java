71ab80d7960aa7753716c16c338c96ba30d65a30
SamuelSena <74965052+Samuelsena57@users.noreply.github.com>
2021-11-04 19:14:32 -0300

    Add files via upload

---
+++ src/main/java/br/com/samuelhsena/dao/DepartamentoDaoImpl.java
@@ -0,0 +1,10 @@
+package br.com.samuelhsena.dao;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.samuelhsena.domain.Departamento;
+
+@Repository
+public class DepartamentoDaoImpl extends AbstractDao<Departamento, Long> implements DepartamentoDao {
+
+}
